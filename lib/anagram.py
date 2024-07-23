93e50c29757b48c96a8227326017bb8715c13bf4
Nathan Welliver <nwelliver.564@gmail.com>
2024-07-23 17:29:27 -0400

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, words):
+        sorted_word = sorted(self.word)
+        matches = [word for word in words if sorted(word) == sorted_word]
+        return matches
